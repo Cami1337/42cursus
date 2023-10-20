3d6c5d483af38da6c649dd114eeed4cc7fa11178
Cami1337 <142174400+Cami1337@users.noreply.github.com>
2023-10-20 15:45:51 +0200

    Add files via upload

---
+++ libft.a/rep_libft.a/rep_libft.a/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 15:59:34 by lglauch           #+#    #+#             */
+/*   Updated: 2023/10/20 13:21:07 by lglauch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int		j;
+	int		sign;
+	long	result;
+
+	j = 0;
+	sign = 1;
+	while (str[j] == '\t' || str[j] == ' ' || str[j] == '\r'
+		|| str[j] == '\f' || str[j] == '\n' || str[j] == '\v')
+		j++;
+	if (str[j] == '+' || str[j] == '-')
+	{
+		if (str[j] == '-')
+			sign = -1;
+		j++;
+	}
+	result = 0;
+	while (str[j] != 0 && str[j] >= 48 && str[j] <= 57)
+	{
+		result = (result * 10) + (str[j] - '0');
+		j++;
+	}
+	return (result * sign);
+}
