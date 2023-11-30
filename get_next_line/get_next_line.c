f0a5cc3d408dd75905eb449795c3f8e7dc92b5c2
Cami1337 <142174400+Cami1337@users.noreply.github.com>
2023-11-30 15:51:46 +0100

    almost finsihed, probems when read is -1

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,17 +5,104 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/15 16:33:21 by lglauch           #+#    #+#             */
-/*   Updated: 2023/11/15 17:03:47 by lglauch          ###   ########.fr       */
+/*   Created: 2023/11/30 14:14:15 by lglauch           #+#    #+#             */
+/*   Updated: 2023/11/30 15:50:41 by lglauch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_theline(t_list *list)
+{
+	int		str_len;
+	char	*next_str;
+
+	if (!list)
+		return (NULL);
+	str_len = get_strlen(list);
+	next_str = malloc(str_len + 1);
+	if (!next_str)
+		return (NULL);
+	str_cpy(list, next_str);
+	return (next_str);
+}
+
+void	append(t_list **list, char *buffer)
+{
+	t_list	*newnode;
+	t_list	*lastnode;
+
+	lastnode = find_lastnode(*list);
+	newnode = malloc(sizeof(t_list));
+	if (!newnode)
+		return ;
+	if (!lastnode)
+		*list = newnode;
+	else
+		lastnode->next = newnode;
+	newnode->str_buffer = buffer;
+	newnode->next = NULL;
+}
+
+void	create_list(t_list **list, int fd)
+{
+	char	*buffer;
+	int		read_char;
+
+	while (!get_newline(*list))
+	{
+		buffer = malloc(BUFFER_SIZE + 1);
+		if (!buffer)
+			return ;
+		read_char = read(fd, buffer, BUFFER_SIZE);
+		if (!read_char)
+		{
+			free(buffer);
+			return ;
+		}
+		buffer[read_char] = '\0';
+		append(list, buffer);
+	}
+}
+
+void	clear_list(t_list **list)
+{
+	t_list	*cleannode;
+	t_list	*lastnode;
+	int		i;
+	int		j;
+	char	*buffer;
+
+	i = 0;
+	j = 0;
+	buffer = malloc(BUFFER_SIZE + 1);
+	lastnode = find_lastnode(*list);
+	cleannode = malloc(sizeof(t_list));
+	if (!cleannode || !buffer)
+		return ;
+	while (lastnode->str_buffer[i] != '\n' && lastnode->str_buffer[i])
+		i++;
+	if (lastnode->str_buffer[i] == '\n')
+		i++;
+	while (lastnode->str_buffer[i])
+		buffer[j++] = lastnode->str_buffer[i++];
+	buffer[j] = '\0';
+	cleannode->str_buffer = buffer;
+	cleannode->next = NULL;
+	dealloc(list, cleannode, buffer);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static t_list	*list;
+	char			*next_line;
 
-	if (!buffer)
-		buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return (NULL);
+	create_list(&list, fd);
+	if (!list)
+		return (NULL);
+	next_line = get_theline(list);
+	clear_list(&list);
+	return (next_line);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,17 +6,32 @@
 /*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:34:08 by lglauch           #+#    #+#             */
-/*   Updated: 2023/11/15 17:04:55 by lglauch          ###   ########.fr       */
+/*   Updated: 2023/11/30 12:53:40 by lglauch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 1042
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 # include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			*str_buffer;
+	struct s_list	*next;
+}					t_list;
 
 char	*get_next_line(int fd);
+int		get_newline(t_list *list);
+t_list	*find_lastnode(t_list *list);
+void	str_cpy(t_list *list, char *next_line);
+int		get_strlen(t_list *list);
+void	dealloc(t_list **list, t_list *cleannode, char *buffer);
+char	*get_theline(t_list *list);
 
 #endif
