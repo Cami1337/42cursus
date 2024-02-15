90e0e520af5107a07c9d5fa8b737c7151c1057da
Cami1337 <142174400+Cami1337@users.noreply.github.com>
2024-02-15 18:02:53 +0100

    Fixed 4 sort and leaks if stack is already sorted

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 14:02:55 by lglauch           #+#    #+#             */
-/*   Updated: 2024/02/05 15:33:53 by lglauch          ###   ########.fr       */
+/*   Updated: 2024/02/15 17:53:02 by lglauch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,13 @@ void	sort_all(t_stack **stack_a, t_stack **stack_b, t_stack **stack_k)
 	push_back(stack_a, stack_b);
 }
 
+void	free_all_stacks(t_stack *stack_a, t_stack *stack_b, t_stack *stack_k)
+{
+	free_stack(stack_a);
+	free_stack(stack_b);
+	free_stack(stack_k);
+}
+
 t_stack	*push_swap(int argc, char **argv)
 {
 	t_stack	*stack_a;
@@ -78,20 +85,22 @@ t_stack	*push_swap(int argc, char **argv)
 
 	stack_a = NULL;
 	stack_b = NULL;
+	stack_k = NULL;
 	if (argc < 2)
 		return (0);
 	arg_count = check_args(argc, argv, &stack_a);
 	if (is_sorted(&stack_a) == 1)
+	{
+		free_stack(stack_a);
 		return (0);
+	}
 	while (arg_count == 3 && !is_sorted(&stack_a))
 		ft_threeargs(&stack_a);
-	while (arg_count == 5 && !is_sorted(&stack_a))
+	while ((arg_count == 5 || arg_count == 4) && !is_sorted(&stack_a))
 		ft_fiveargs(&stack_a, &stack_b);
 	while (arg_count > 5 && !is_sorted(&stack_a))
 		sort_all(&stack_a, &stack_b, &stack_k);
-	free_stack(stack_a);
-	free_stack(stack_b);
-	free_stack(stack_k);
+	free_all_stacks(stack_a, stack_b, stack_k);
 	return (stack_a);
 }
 
