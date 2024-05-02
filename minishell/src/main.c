bbedd161a0115cc89e187f1e501ff2ef46ff6dec
Cami1337 <leoglauch@t-online.de>
2024-05-02 19:56:39 +0200

    start

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -3,11 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
+/*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 17:51:26 by lglauch           #+#    #+#             */
-/*   Updated: 2024/04/29 17:51:32 by lglauch          ###   ########.fr       */
+/*   Updated: 2024/05/02 19:55:56 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    printf("\n");
+    printf("   _   _   _   _   _   _   _   _   _   \n");
+    printf("  / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ \n");
+    printf(" ( M | I | N | I | S | H | E | L | L )\n");
+    printf("  \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \n");
+    printf("********************************\n");
+    printf("\nWelcome to Minishell by \033]8;;https://github.com/lglauch\alglauch\033]8;;\a\n\n");
+    printf("********************************\n");
+    while(1) {
+        printf("🐚  ");
+        read = getline(&line, &len, stdin);
+
+        if (read == -1) {
+            perror("getline");
+            exit(EXIT_FAILURE);
+        }
+        line[strcspn(line, "\n")] = 0;
+        if(strcmp(line, "exit") == 0) {
+            break;
+        }
+    }
+    free(line);
+    return 0;
+}
